197c2ca40c514d7e03befcfbc9a5e8ecdd6452fe
Sultan9 <msultan49@outlook.com>
2020-06-28 18:55:57 +0300

    added modal on user click

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -16,7 +16,43 @@ class ModalComponent extends React.Component {
         return (
             <div ref={this.myRef} className="modal-container" onClick={this.props.closeModal}>
                 <div className="modal-text">
-                    {this.props.data && this.props.data.firstName
+                    {this.props.data &&
+                        <div className="profile">
+                            <div className="text-center">
+                                <h4>{this.props.data.firstName} {this.props.data.lastName}, {this.props.data.age}</h4>
+                                <h6>{this.props.data.nationality}</h6>
+                            </div>
+                            <div className="info">
+                                <div>
+                                    <i className="fa fa-address-card"></i>
+                                    <div className="info-text">
+                                        <div>Address</div>
+                                        <div>{this.props.data.address}</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <i className="fa fa-phone"></i>
+                                    <div className="info-text">
+                                        <div>Phone Number</div>
+                                        <div>{this.props.data.phone}</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <i className="fa fa-list-ol"></i>
+                                    <div className="info-text">
+                                        <div>Services Subscribed</div>
+                                        <div>{this.props.data.services}</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <i className="fa fa-money-bill"></i>
+                                    <div className="info-text">
+                                        <div>Last Month's Bill</div>
+                                        <div>{this.props.data.lastMonthBill}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     }
                 </div>
             </div>
